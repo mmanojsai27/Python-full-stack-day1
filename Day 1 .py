8f17caa024e8f2979a48a6f5f511946a221ead90
Manoj Sai <134284439+mmanojsai27@users.noreply.github.com>
2026-06-27 12:31:15 +0530

    Add files via upload

---
+++ Day 1 .py
@@ -0,0 +1,123 @@
+'''Python Notes
+
+''
+Procedural Language
+-------------------
+-->This follows a step by step approach, where code is structure into procedure such as function or routines...
+
+eg--> C
+
+OOP'S
+---------
+--> This is based on concepts of object and class..'''
+''''
+Example:
+print(89 + 67)
+num = 89
+num_2 = 67
+total_ num + Num_2
+print(id(num))
+print(id(num_2))
+print(id(total_))
+
+Python
+----------------
+1.dynamically typed
+--------------------
+--> Python know the type of data we are passing to the variable...
+---------------
+2.interperter
+---------------
+--->Python excute code line-by-line , if any error it will stop the execution in that and before will gives the output...
+example:
+num = 89
+print(type(num))
+num_2 = 67
+total_ num + num_2
+print(id(num))
+print(id(num_2))
+
+print(total_)
+
+---------------
+3.High_level
+--------------
+--->
+
+
+
+
+
+------------------
+---> 1991 --> Guide Van Russum he is invited the python
+
+Why we use python?
+-----------------------------
+-->Easy syntax
+-->cross-platform
+--->wide applications
+-->Huge Library support
+
+Tokens
+---------------------------
+keywords
+---------------------------
+-->There are reserved words in python
+Example: if , else, for, while
+
+2.Identifires
+--------------------------
+--->Name given to variables, functions, class
+
+3.Literals
+------------------------
+--->89,"hello", 4.56
+
+4.operators
+--------------------
+---> + - * /
+
+5.Punctuators
+--------------------------
+--->(), {} ,[]...
+
+
+===================================
+Rules of Variables
+=================================
+Goods Ways to DO
+A-Z, a-z, _ .....
+Example's:
+num_1 = 7
+num = 8
+Num_2 = 9
+----------------------------
+Bad Ways Of writings 
+---------------------------
+Special char, number, space 
+$num = 0
+su@m = 8
+s o = 89
+2num = 9
+==============================
+Comments
+==========================
+1.Single line comments
+---> " # "
+Example: num = 9
+if num % 2 == 0; #Checking a number is even or odd
+        print("Even")
+else:
+     print("odd")
+2. Multi line comments
+--->''' ''' , """ """
+---------------------------------------
+Boolean
+---------------
+True
+False
+
+
+
+
+
