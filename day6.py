2551d14201f16a8c0bf4c4e8742a2b51e50a1df5
Manoj Sai <134284439+mmanojsai27@users.noreply.github.com>
2026-06-30 12:21:07 +0530

    Add files via upload
    Day 6 Python notes covering type conversion between integers, floats, strings, lists, tuples, and dictionaries using built-in functions with examples.
    
    This repository contains my Day 6 Python learning notes on Type Conversion (Type Casting) in Python. It covers converting between different data types such as integers, floats, strings, lists, tuples, and dictionaries using built-in functions like int(), float(), str(), list(), tuple(), and dict(). The repository also includes practical examples demonstrating both implicit and explicit type conversions to help understand data manipulation in Python.

---
+++ day6.py
@@ -0,0 +1,109 @@
+#Type convertions
+"""
+This is process of converting one data type to another..
+-------------------------------------------------------------
+int - string,float
+--------------------------------------------------------------
+eg:-
+num = 89
+num_2 = float(num)
+print(num_2)
+print(type(num))
+so = str(num)
+print(type(num))
+
+-------------------------------------------------------------
+Float: string;interger
+-----------------------------------------
+eg:-
+num = 8.9
+how = str(num)
+print(how)
+print(type(how))
+num_2 = int(num)
+print(num_2)
+-------------------------------------------------------------------------
+String: Interger ; Float ; List ; tuple;
+-----------------------------------------------------------
+*hai = "78"
+num = int(hai)
+print(num + 10)
+
+hello = "67.89"
+num_2 = float(hello)
+
+print(num_2 + num) upto float.
+
+**any_ = "456789"
+con_ = list(any_)
+print(con_)
+
+con_2 = tuple(any_)
+print(con_2)
+-------------------------------------------------------
+List: String ; Tuples ;Dictionary
+---------------------------------------------------------
+*var = ['p','y','t','h','o','n']
+text = "".join(var)
+print(text)
+
+**some = tuple(var)
+print(some)
+
+***dict_1 = [('a',1),('b',8)]
+convert = dict(dict_1)
+print(dict_1)
+-----------------------------------------------------
+Tuple: String ; Lists.
+-----------------------------------------
+*fam = ('h','e','l','l','o')
+text_2 = "".join(fam)
+print(text_2)
+
+**
+--------------------------
+Built-in-functions
+--------------------------
+str()
+float()
+int()
+list()
+dict()
+
+
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
